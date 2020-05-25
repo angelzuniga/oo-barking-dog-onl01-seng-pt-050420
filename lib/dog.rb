538bd3461a4636ce0df1c253dd924bb752f0412a
Angel Zuniga <angelzuniga@outlook.com>
2020-05-25 05:26:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,16 @@
-class Dog 
-  def dog.new(dog_name) 
-  @dog = dog_name
-end
-end
-dog(fido)
+# class Dog 
+#   def dog.new(dog_name) 
+#   @dog = dog_name
+# end
+# end
+# dog(fido)
 
+class Dog
+  def name=(dog_name)
+  this_dogs_name = dog_name
+end
+
+def name
+  this_dogs_name
+  end
+end
